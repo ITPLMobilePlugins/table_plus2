487adeb6ae3cece0fadd04befc0a651254109ede
ajeethhacki <sankarrajajeethkumar@gmail.com>
2022-01-21 10:45:49 +0530

    Added desc. removed commented lines

--- lib/table_plus.dart
+++ lib/table_plus.dart
@@ -195,25 +195,16 @@ class _TablePlusState extends State<TablePlus> {
         successMsg();
       }
     } else if (kIsWeb) {
-      //now convert our 2d array into the csvlist using the plugin of csv
       String csv = const ListToCsvConverter().convert(dataValue);
-//this csv variable holds entire csv data
-//Now Convert or encode this csv string into utf8
       final bytes = utf8.encode(csv);
-//NOTE THAT HERE WE USED HTML PACKAGE
       final blob = html.Blob([bytes]);
-//It will create downloadable object
       final url = html.Url.createObjectUrlFromBlob(blob);
-//It will create anchor to download the file
       final anchor = html.document.createElement('a') as html.AnchorElement
         ..href = url
         ..style.display = 'none'
         ..download = '${widget.exportFileName}.csv';
-//finally add the csv anchor to body
       html.document.body?.children.add(anchor);
-// Cause download by calling this function
       anchor.click();
-//revoke the object
       html.Url.revokeObjectUrl(url);
       successMsg();
     }
